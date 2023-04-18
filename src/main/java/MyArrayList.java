136f37e63d24765a9a309995e3fd6e60485145eb
Rohini.Raman <rohini.raman@gdit.com>
2023-04-18 12:24:28 -0500

    Initial commit

---
+++ src/main/java/MyArrayList.java
@@ -0,0 +1,11 @@
+
+
+public interface MyArrayList {
+    void add(Object ob);
+    Object get(int index);
+    boolean contains(Object element);
+    boolean isEmpty();
+    int size();
+    //Implementation of this API is optional
+    boolean remove(Object element);
+}
---
+++ src/test/java/MyArrayListTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyArrayListTest {
+
+    @Test
+    void testSize() {
+         //MyArrayList myArrayList = new MyArrImpl();
+         //assertEquals(0, myArrayList.size());
+    }
+
+}
+
+
+
+
